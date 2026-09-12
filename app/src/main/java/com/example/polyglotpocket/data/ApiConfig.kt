2faa834082850b61931d668316ca4038dae6a64d
lindalabonia <linda.lab94@gmail.com>
2026-09-13 00:38:43 +0200

    update apiConfig.kt

--- app/src/main/java/com/example/polyglotpocket/data/ApiConfig.kt
+++ app/src/main/java/com/example/polyglotpocket/data/ApiConfig.kt
@@ -1,7 +1,6 @@
 package com.example.polyglotpocket.data
 
 object ApiConfig {
-    // TODO: replace with YOUR PythonAnywhere address, with NO trailing slash.
-    // Example: "https://linda.pythonanywhere.com"
+    // PythonAnywhere address
     const val BASE_URL = "https://omarallam95.eu.pythonanywhere.com"
 }
